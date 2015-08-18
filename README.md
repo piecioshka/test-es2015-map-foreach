dac45692c0aac8c1d69ef8070272a672feeb7a91
Piotr Kowalski <piecioshka@gmail.com>
2015-08-18 20:32:05 +0200

    Create simple code to check that Map#forEach is working.

---
+++ README.md
@@ -0,0 +1,6 @@
+# test-es6-set
+
+More:
+
+ - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/forEach
+ - https://kangax.github.io/compat-table/es6/#Map_Map.prototype.forEach
---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let m = new Map();
+let o = [1, 2];
+let v = [];
+
+m.set('foo', 1);
+m.set('bar', 2);
+
+m.forEach(function (item) {
+    v.push(item);
+});
+
+if (String(v) == String(o)) {
+    console.info('Map#forEach is working.');
+} else {
+    console.error('Map#forEach is not working yet.');
+}
